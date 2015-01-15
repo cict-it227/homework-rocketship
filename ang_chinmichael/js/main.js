a7b328ec37f31c65de13117d9564348a57a28a6d
Chin Michael Ang <cm_ang@wvsu.edu.ph>
2015-01-15 23:24:26 +0800

    Update main.js

--- ang_chinmichael/js/main.js
+++ ang_chinmichael/js/main.js
@@ -5,7 +5,7 @@
 window.onload = function () 
 {
 	//initialize game
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 	function preload() 
 	{
 
