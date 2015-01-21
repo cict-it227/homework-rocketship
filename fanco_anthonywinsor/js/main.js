d64d5456660edcde54c4887ccf073a9e61b4d344
wewinz09 <wewinz09@ymail.com>
2015-01-21 20:30:44 +0800

    ProjectLupad
    ProjectLupad

--- fanco_anthonywinsor/js/main.js
+++ fanco_anthonywinsor/js/main.js
@@ -1,24 +1,49 @@
-//the getting started guide can be found here: https://github.com/photonstorm/phaser/tree/master/resources/tutorials
-
-//get window size using jquery
-var winh = $(window).height()-20;
-var winw = $(window).width()-20;
+//Anthony Winsor S. Fanco
+//BSIT - 4A
 
 //call the main function when the browser starts
-window.onload = function() {
+window.onload = function() 
+	{
     //initialize game
-    var game = new Phaser.Game(winw, winh, Phaser.AUTO, '', { preload: preload, create: create });
-    
-    //pre-define game assets and settings
-    function preload () {
-        game.load.image('rocket', 'assets/rocket.png');
-        game.stage.backgroundColor = '#fff';
+	var game = new Phaser.Game(715, 480, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+function preload() {
+    game.load.image('sky', 'assets/background.png');
+    game.load.image('rocket', 'assets/ship.png');
+}
+
+var rocket;
+
+function create() {
+
+	game.add.sprite(0, 0, 'sky');
+    rocket = game.add.sprite(game.world.centerX, game.world.centerY, 'rocket');
+    rocket.anchor.setTo(0.5, 0.5);
+    rocket.scale.setTo(0.5, 0.5);
+
+
+}
+
+function update() {
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        rocket.x -= 10;
     }
-    
-    //call game assets and set its properties
-    function create () {
-        var rocket = game.add.sprite(game.world.centerX, game.world.centerY, 'rocket');
-        rocket.anchor.setTo(0.5, 0.5);
-        rocket.scale.setTo(0.5,0.5)
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        rocket.x += 10;
     }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
+    {
+        rocket.y -= 10;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+    {
+        rocket.y += 10;
+    }
+}
+
+
 };
